abe1eb086b5022e317c4ee7deb8afe306301a6ad
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-03-29 21:29:59 +0700

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,5 +1,4 @@
 import os
-import sys
 from glob import glob
 from typing import Any, Dict, List, Union
 
@@ -80,39 +79,4 @@ def get_languages() -> Dict[str, Union[str, List[str]]]:
         }
         for code in languages
     }
-
-import os
-from typing import List
-
-import yaml
-
-languages = {}
-languages_present = {}
-
-
-def get_string(lang: str):
-    return languages[lang]
-
-
-for filename in os.listdir(r"./strings/langs/"):
-    if "en" not in languages:
-        languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
-        )
-        languages_present["en"] = languages["en"]["name"]
-    if filename.endswith(".yml"):
-        language_name = filename[:-4]
-        if language_name == "en":
-            continue
-        languages[language_name] = yaml.safe_load(
-            open(r"./strings/langs/" + filename, encoding="utf8")
-        )
-        for item in languages["en"]:
-            if item not in languages[language_name]:
-                languages[language_name][item] = languages["en"][item]
-    try:
-        languages_present[language_name] = languages[language_name]["name"]
-    except:
-        print("There is some issue with the language file inside bot.")
-        exit()
-        
+    
