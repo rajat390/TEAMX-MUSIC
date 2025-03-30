738101e94b12c0422417e7db93cf151a38480fce
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-03-30 12:48:23 +0700

    Update ping.py

--- AnonXMusic/plugins/tools/ping.py
+++ AnonXMusic/plugins/tools/ping.py
@@ -5,7 +5,7 @@ from pyrogram.types import Message
 
 from AnonXMusic import app, Userbot as Client
 from AnonXMusic.core.call import Anony
-from AnonXMusic.core.userbot import Userbot as Xbot
+#from AnonXMusic.core.userbot import
 from AnonXMusic.utils import bot_sys_stats
 from AnonXMusic.utils.decorators.language import language
 from AnonXMusic.utils.inline import supp_markup
@@ -30,17 +30,16 @@ async def ping_com(client, message: Message, _):
         reply_markup=supp_markup(_),
     )
 
-@Xbot.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
-@language
+@app.on_message(filters.command("ping"))
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_video(
         video=PING_IMG_URL,
         caption=_["ping_1"].format(app.mention),
     )
-    pytgping = await Xbot.ping()
+    pytgping = await app.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
-        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),)
+        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping))
     
